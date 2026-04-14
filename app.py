f819699917e0192f8bb8ec2598fa78df15b2ce4e
Ivaylo763 <iedzhurelov24a@gmail.com>
2026-04-14 14:28:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,2 @@
-
+import streamlit as st
+bad_ingredients = ["Е102", "Тартразин", "Tartrazine", "Е110", "Сънсет Йелоу", "Sunset Yellow", "Е120", "Кошенил", "Cochineal", "Е220", "Серен диоксид", "Sulfur dioxide", "Е250", "Натриев нитрит", "Sodium nitrite", "Е621", "Мононатриев глутамат", "Monosodium glutamate", "Е951", "Аспартам", "Aspartame" ]
